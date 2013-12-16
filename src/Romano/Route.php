a832a334e399cbe9aae4146c52810805bdf13d7b
Thijzer <Thijs.dp@gmail.com>
2013-12-16 19:07:49 +0100

    composer

---
+++ src/Romano/Route.php
@@ -0,0 +1,61 @@
+<?php
+class Route
+{
+  var $r = array();
+
+  function __construct($url, $paths)
+  {
+  	$this->finder($url, $paths);
+  }
+  private function finder($url, $paths)
+  {
+    // count,trim, dissect the given url into sections, 1 ctrlr 2 method
+    $pos = count($section = list($ctrlr, $method) = explode('/', $url = trim($url,'/')));
+    // find the missing values first = nothing = home/index ,2nd find physical index and last we latch on a home as ctrlr.
+    if (!$method) {
+      if (!$ctrlr) {
+        $ctrlr = 'home'; $method = 'index';
+      } elseif (file_exists(VIEW.$ctrlr.'/index.php')) {
+        $method = 'index';
+      } else {
+        $method = $ctrlr; $ctrlr = 'home';
+      }
+    }
+    // now all values are set lets compare against the paths array
+    if ($array = $paths[$ctrlr][$method]){
+      $this->r = array('method' => $method, 'controller' => $ctrlr, 'url' => $url, 'section' => $section, 'pos' => $pos, 'path' => $ctrlr.'/'.$method);
+      //dump($this->r);die();
+      $this->autoRoute($array);
+    } else {
+    // no array no go we give them the 404;
+      View::page(404,'from Route');
+    }
+  }
+  private function autoRoute($path)
+  {
+    if (!is_array($path) && empty($this->r['section'][2]))  {
+      View::render($this->r);
+    } else {
+      foreach ($path as $set => $rules) {
+        if ($set[0] === '$' && !empty($this->r['section'][2])) {
+          $rules[substr($set, 1)] = $this->r['section'][2];
+          // now lets grab the rules stick and them in the data container
+          if (is_array($rules) ) {
+            if ($rules['type'] === 'numeric' && !is_numeric($this->r['section'][2]) ) {
+              View::page(404,'from Route');
+              exit();
+            }
+            $this->r = array_merge($rules, $this->r);
+          }
+        }
+      }
+      if (file_exists (CONTROLLER.$this->r['controller'].EXT)) {
+        require (CONTROLLER.$this->r['controller'].EXT);
+        $class = ucfirst($this->r['controller']);
+        $init = new $class();
+        $init->{$this->r['method']}($this->r);
+        exit();
+      }
+    }
+  }
+}
---
+++ src/Romano/Validate.php
@@ -0,0 +1,106 @@
+<?php
+/*
+  * Validation library 11/2013
+  * --------------------------
+  * creator/source : Alex Garrett
+  * - added regex
+  * - decoupled
+  * - CheckDB method
+  * - gate system for less validations
+  *
+  * dependencies : needs DB layer/wrapper for "checkDB"
+  */
+class Validate
+{
+  private $gate;
+
+  private $errors = array(),
+          $data = array();
+
+  public function check($source, $items = array() )
+  {
+    foreach ($items as $item => $rules) {
+      $this->gate = true;
+      foreach ($rules as $rule => $rule_value) {
+        $value = trim($source[$item]);
+        if ($this->gate === true) {
+          switch ($rule) {
+            case 'required':
+              if (empty($value) AND $rule_value === true ) {
+                $this->addError("{$item} is required ");
+              }
+              break;
+            case 'min':
+              if (strlen($value) < $rule_value) {
+                $this->addError("{$item} must have a minimum of {$rule_value} characters ");
+              }
+              break;
+            case 'max':
+              if (strlen($value) > $rule_value) {
+                $this->addError("{$item} must have a maximum of {$rule_value} characters ");
+              }
+              break;
+            case 'matches':
+              if ($value !== $source[$rule_value]) {
+                $this->addError("{$item} has no match ");
+              }
+              break;
+            case 'regex':
+              if (!preg_match($rule_value, $value) ) {
+                $this->addError("{$item} has no character match ");
+              }
+              break;
+            case 'db':
+              if (is_array($rule_value)) {
+                $this->checkDB($rule_value, strtolower($item), strtolower($value));
+              }
+              break;
+            default:
+              $this->addError("{$rule} is not an active rule ");
+              break;
+          }
+        }
+      }     
+      if (!$this->errors) {
+        $this->data[$item] = $value;
+      }
+    }
+  }
+  public function checkDB($array, $where_key, $where_val)
+  {
+    if ($result = DB::connect($array['table'])->get(array($where_key => $where_val))->fetch() ) {
+      foreach ($array as $key => $value) {
+        switch ($key) {
+          case 'unique':
+            if ($result AND $value === true) {
+              $this->addError($where_key . ' exists ');
+            }
+            break;
+          case 'active':
+            if ($result['active'] === 1 AND $value === false) {
+              $this->addError($where_key . " is activated ");
+            }elseif (empty($result['active']) AND $value === true) {
+              $this->addError($where_key . " is not activated ");
+            }
+            break;        
+        }
+      }
+      if (!$this->errors) {
+        $this->data[$where_key] = $where_val;
+      }
+    }
+  }
+  private function addError($error)
+  {
+    $this->errors[] = $error;
+    $this->gate = false;
+  }
+  public function errors()
+  {
+    return $this->errors;
+  }
+  public function getField($item)
+  {
+    return $this->data[$item];
+  }
+} 
